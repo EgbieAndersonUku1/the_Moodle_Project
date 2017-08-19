83325ec66e15fccf315245a062c8300e19842d62
jayunderwood2011 <jayunderwood2011@hotmail.com>
2017-08-19 14:37:41 +0100

    Refactored a few lines of code in test cases

--- test_scenerios/tests/login_tests.py
+++ test_scenerios/tests/login_tests.py
@@ -8,8 +8,7 @@ from time import sleep
 # Uses Pytest to test login test cases
 # TODO
 # Works perfectly on Linux but on Window opens an insecure password page tab -> Fix that.
-# Fix the assert statement for logout because if a test fails then all tests that follow will fail because
-# the application logout line is not executed.
+
 
 class TestLogin(TestCase):
     """Test cases for the login a user """
@@ -19,6 +18,7 @@ class TestLogin(TestCase):
         cls.moodle = Moodle(url)
         cls._valid_username = 'course creator username'
         cls._valid_password = 'course creator password'
+        cls._text = {}
 
     @classmethod
     def tearDownClass(cls):
@@ -35,25 +35,42 @@ class TestLogin(TestCase):
         cls._login_helper('1234', '1234')
 
     def test_Given_that_a_user_logs_in_with_a_valid_username_and_a_valid_password__Should_login_user_and_display_home_page(cls):
-        cls._login_helper(cls._valid_username, cls._valid_password, 'Novus Community Management System')
-        cls.moodle.Users.CourseCreator.Browser.Pages.GoTo.LoginPage.Logout.log_out()
+        cls._login_helper(cls._valid_username, cls._valid_password)
+        cls._check_test_result_and_logout('Novus Community Management System')
 
     def test_user_logs_in_with_not_case_sensitive_username_and_correct_password__Should_login_user_and_display_home_page(cls):
-        cls._login_helper(cls._valid_username.upper(), cls._valid_password, 'Novus Community Management System')
-        cls.moodle.Users.CourseCreator.Browser.Pages.GoTo.LoginPage.Logout.log_out()
+        cls._login_helper(cls._valid_username.upper(), cls._valid_password)
+        cls._check_test_result_and_logout('Novus Community Management System')
 
     def test_guest_hits_the_guest_login_button__Should_log_user_in_as_guest_and_display_home_page(cls):
-        cls._login_helper(text='Novus Community Management System', guest=True)
-        cls.moodle.Users.Guest.Browser.Pages.GoTo.LoginPage.Logout.log_out()
+        cls._login_helper(guest=True)
+        cls._check_test_result_and_logout('Novus Community Management System', logout_as_guest=True)
 
-    def _login_helper(cls, username='', password='', text='Log in to the site', guest=False):
+    def _login_helper(cls, username='', password='', guest=False):
         """A thin wrapper function that adds additional help to test methods"""
 
         cls.moodle.username, cls.moodle.password = username, password
         cls.moodle.initalise()
 
         if not guest:
-           cls.moodle.Users.CourseCreator.Browser.Pages.GoTo.LoginPage.Login.UserLogin.login()
+            cls.moodle.Users.CourseCreator.Browser.Pages.GoTo.LoginPage.Login.UserLogin.login()
         else:
             cls.moodle.Users.Guest.Browser.Pages.GoTo.LoginPage.Login.LoginAs.guest()
-        cls.assertEquals(text, cls.moodle.get_page_title())
+
+    def _check_test_result_and_logout(cls, actual_text='Log in to the site', logout_as_guest=False):
+
+        cls._store_expected_results(actual_text)
+        cls._logout(logout_as_guest)
+        cls.assertEquals(cls._text.get('actual_text'), cls._text.get('page_text'))
+
+    def _store_expected_results(cls, actual_text):
+
+        cls._text['actual_text'] = actual_text
+        cls._text['page_text'] = cls.moodle.get_page_title()
+
+    def _logout(cls, guest=False):
+
+        if guest:
+            cls.moodle.Users.Guest.Browser.Pages.GoTo.LoginPage.Logout.log_out()
+        else:
+            cls.moodle.Users.CourseCreator.Browser.Pages.GoTo.LoginPage.Logout.log_out()
